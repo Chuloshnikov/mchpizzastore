8d497535699bf6d7ee81b2dd79a665627695d1ee
MaxChuloshnikov <Maks447@ukr.net>
2023-04-08 21:01:59 +0300

    pizzacart done

--- src/components/PizzaList.tsx
+++ src/components/PizzaList.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Item } from "../../type";
 import PizzaCard from './PizzaCard';
 
-const PizzaList = () => {
+const PizzaList = ({ productData }: any) => {
   return (
-    <div id="products" className='max-w-contentContainer mx-auto mt-6'>
+    <div id="products" className='max-w-contentContainer mx-auto mt-6 mb-6'>
       <div className='text-center'>
           <h2 className='text-yellow-950 font-bold text-2xl'>The Best Pizza</h2>
           <p className='text-yellow-950 font-semibold text-lg'>
@@ -13,16 +14,11 @@ const PizzaList = () => {
           </p>
       </div>
       <div className='className="py-6 px-4 grid xs:grid-cols-1 md:grid-cols-2 mdl:grid-cols-2 lgl:grid-cols-4 gap-4 mt-6'>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
+        {productData.map((item: item) => (
+          <PizzaCard item={item}/>
+        )
+        
+        )}
       </div>  
     </div>
   )
--- src/pages/api/productdata.ts
+++ src/pages/api/productdata.ts
@@ -1,13 +1,101 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {pizza1} from '../../assets/index';
 
 type Data = {
-  name: string
+  _id: number,
+  title: string,
+  description: string,
+  price: number[],
+  img: string,
 }
+
+const productData = [
+  {
+  _id: 1,
+  title: "GORGONZOLA",
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Accusamus corporis sequi 
+  nesciunt ab pariatur maiores quo aliquam quis, officiis, 
+  consequuntur eius rerum adipisci? Nemo suscipit aliquid rerum deserunt eveniet veniam?`,
+  price: [19.99, 22.99, 27.99],
+  img: pizza1
+  },
+  {
+    _id: 2,
+    title: "GORGONZOLA",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Accusamus corporis sequi 
+    nesciunt ab pariatur maiores quo aliquam quis, officiis, 
+    consequuntur eius rerum adipisci? Nemo suscipit aliquid rerum deserunt eveniet veniam?`,
+    price: [19.99, 22.99, 27.99],
+    img: pizza1
+    },
+    {
+      _id: 3,
+      title: "GORGONZOLA",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Accusamus corporis sequi 
+      nesciunt ab pariatur maiores quo aliquam quis, officiis, 
+      consequuntur eius rerum adipisci? Nemo suscipit aliquid rerum deserunt eveniet veniam?`,
+      price: [19.99, 22.99, 27.99],
+      img: pizza1
+      },
+      {
+        _id: 4,
+        title: "GORGONZOLA",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Accusamus corporis sequi 
+        nesciunt ab pariatur maiores quo aliquam quis, officiis, 
+        consequuntur eius rerum adipisci? Nemo suscipit aliquid rerum deserunt eveniet veniam?`,
+        price: [19.99, 22.99, 27.99],
+        img: pizza1
+        },
+        {
+          _id: 5,
+          title: "GORGONZOLA",
+          description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Accusamus corporis sequi 
+          nesciunt ab pariatur maiores quo aliquam quis, officiis, 
+          consequuntur eius rerum adipisci? Nemo suscipit aliquid rerum deserunt eveniet veniam?`,
+          price: [19.99, 22.99, 27.99],
+          img: pizza1
+          },
+          {
+            _id: 6,
+            title: "GORGONZOLA",
+            description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Accusamus corporis sequi 
+            nesciunt ab pariatur maiores quo aliquam quis, officiis, 
+            consequuntur eius rerum adipisci? Nemo suscipit aliquid rerum deserunt eveniet veniam?`,
+            price: [19.99, 22.99, 27.99],
+            img: pizza1
+            },
+            {
+              _id: 7,
+              title: "GORGONZOLA",
+              description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Accusamus corporis sequi 
+              nesciunt ab pariatur maiores quo aliquam quis, officiis, 
+              consequuntur eius rerum adipisci? Nemo suscipit aliquid rerum deserunt eveniet veniam?`,
+              price: [19.99, 22.99, 27.99],
+              img: pizza1
+              },
+              {
+                _id: 8,
+                title: "GORGONZOLA",
+                description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Accusamus corporis sequi 
+                nesciunt ab pariatur maiores quo aliquam quis, officiis, 
+                consequuntur eius rerum adipisci? Nemo suscipit aliquid rerum deserunt eveniet veniam?`,
+                price: [19.99, 22.99, 27.99],
+                img: pizza1
+                },
+]
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(productData)
 }
