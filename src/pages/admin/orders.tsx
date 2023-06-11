ad1640a1454cb731a355cff7e56b0d26d28fd60f
MaxChuloshnikov <Maks447@ukr.net>
2023-06-12 01:44:05 +0300

    Admin pages done

---
+++ src/pages/admin/orders.tsx
@@ -0,0 +1,14 @@
+import AdminLayout from '@/components/AdminLayout';
+import React from 'react'
+
+const Orders = () => {
+  return (
+    <AdminLayout>
+        <div>
+            orders
+        </div>
+    </AdminLayout>
+  )
+}
+
+export default Orders;
