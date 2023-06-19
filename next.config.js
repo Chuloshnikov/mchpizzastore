27436900e734a2a866c856b3bc7c0c3102a56b86
MaxChuloshnikov <Maks447@ukr.net>
2023-06-19 18:39:26 +0300

    done

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ["i.ibb.co", "images.pexels.com", "lh3.googleusercontent.com", "res.cloudinary.com", "craigourparkprimary.files.wordpress.com"],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 }
 
 module.exports = nextConfig
